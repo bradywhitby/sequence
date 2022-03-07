d7c9f8c965595635b1bf9f2fa81403841e8a9bb8
Brady Whitby <bradywhitby@gmail.com>
2022-03-07 15:25:13 +1000

    impemented __name__ == __main__ module protection and implemented __len__ method

--- sequence.py
+++ sequence.py
@@ -3,6 +3,18 @@ The sequence module takes a pecified sequence
 and provides an iterable implementatin to loop through
 the specified sequence
 '''
+
+SEQ_TYPES = [
+    "alpha_numerical_both_case",
+    "alpha_numerical_lower_case",
+    "alpha_numerical_upper_case",
+    "alpha_lower_case",
+    "alpha_upper_case",
+    "numerical",
+    "hex",
+    "custom"
+]
+
 class Seq:
 
 
@@ -36,6 +48,11 @@ class Seq:
             'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4',
             '5', '6', '7', '8', '9'
         ]
+
+        # converts string to list
+        if isinstance(sequence_list, str):
+            list(sequence_list)
+
         self.sequence_map = {
             "alpha_numerical_both_case": self.__alpha_num,
             "alpha_numerical_lower_case": self.sublist('a','z','0','9'),
@@ -49,21 +66,28 @@ class Seq:
 
         if sequence_type not in self.sequence_map.keys():
             raise ValueError("ValueError thrown. Argument \"sequence_type\" \
-                must be in this list:\n{}".format(self.sequence_map.keys()))
+                must be in this list:\n{}".format(SEQ_TYPES))
         if sequence_type == "custom" \
                 and (sequence_list is None \
                     or not isinstance(sequence_list, list)
-                    or (isinstance(sequence_list, list)
+                    or not isinstance(sequence_list, str)
+                    or (
+                        (isinstance(sequence_list, list)
+                            or isinstance(sequence_list, str)
+                        )
                         and len(sequence_list) < 2
                     )
                 ):
             raise ValueError("ValueError thrown. Argument \"sequence_list\" must \
-                contain a list of at least 2 strings when sequence_type == \"custom\"")
+                contain a list of at least 2 strings elements or a string of at least 2 characters \
+                when sequence_type == \"custom\"")
 
         self.sequence_list = self.sequence_map[self.sequence_type]
         self.start_sequence = start_sequence
         self.current_sequence = start_sequence
         self.end_sequence = end_sequence
+
+        self.length = ""
 
     def __iter__(self):
         return self
@@ -78,7 +102,7 @@ class Seq:
         ''''
         Sets self.current_sequence to the next sequence
 
-        Returns slef.current_sequence
+        Returns self.current_sequence
         '''
         index_pos = [self.sequence_list.index(str(char)) for char in self.current_sequence]
 
@@ -107,3 +131,58 @@ class Seq:
                 self.__alpha_num.index(args[1]) + 1]
         return self.__alpha_num[self.__alpha_num.index(args[0]):
             self.__alpha_num.index(args[1]) + 1] + self.sublist(*args[2:])
+
+    def __len__(self):
+        return self.get_length()
+
+    def get_length(self):
+        '''
+        Helper function to calculate the number of variations between the start and end sequence
+        '''
+        #101 = (1 × 2^0) + (0 × 2^1) + (1 × 2^2) = 5.
+        #(value x base array length ^ index pos)
+        if self.end_sequence is None:
+            return ValueError
+
+        num_list = []
+        start_num = 0
+        end_num = 0
+
+        # get the value of each character
+        for i in range(len(self.start_sequence)):
+            start = self.start_sequence[i]
+            start_index = self.sequence_list.index(start)
+
+            end = self.end_sequence[i]
+            end_index = self.sequence_list.index(end)
+
+            num_entry = [start_index, end_index]
+            num_list.append(num_entry)
+
+        # reverse the list as bases conversion goes from right to left
+        num_list.reverse()
+        # get the total numerical value of the start and end sequence
+        for j, value in list(enumerate(num_list)):
+            start_num += (value[0] * len(self.sequence_list)**j)
+            end_num += (value[1] * len(self.sequence_list)**j)
+
+        return end_num - start_num
+
+if __name__ == "__main__":
+    start_seq = input("Start sequence: ")
+    end_seq = input("End sequence: ")
+    print("")
+    for seq_types_index, seq_types_value in enumerate(SEQ_TYPES):
+        print("{}: {}".format(seq_types_index, seq_types_value))
+    seq_type = input("\nPlease enter a sequence type from the above selection\n\
+   # you can enter the string or the corrisponding number: ")
+    if seq_type.strip().isdigit():
+        seq_type = SEQ_TYPES[int(seq_type)]
+    seq_list = []
+    if seq_type == "custom":
+        seq_list = input("Please enter a sequence string or list: ")
+    seq = Seq(start_seq, end_seq, seq_type, seq_list)
+    print(seq.current_sequence)
+    for each in seq:
+        print(each)
+    seq.get_length()
