d9f474dea4607f902a11c679fdf9db4aadbffd6e
Brady Whitby <bradywhitby@gmail.com>
2022-03-08 17:23:18 +1000

    Updated constructor doctring

--- sequence.py
+++ sequence.py
@@ -26,10 +26,10 @@ class Seq:
 
     def __init__(
         self,
-        start_sequence,
-        end_sequence=None,
-        sequence_type="alpha_numerical_both_case",
-        sequence_list=None,
+        start_sequence: str,
+        end_sequence: str | None = None,
+        sequence_type: str = "alpha_numerical_both_case",
+        sequence_list: list | str | None = None,
     ):
         """
         An iterable class to generate a specified sequence.
@@ -45,6 +45,19 @@ class Seq:
             ValueError: if sequence_type not in SEQ_TYPES
 
         Seq(str, str, str, [str,]) -> Seq()
+
+        Args:
+            start_sequence (str): the start of the sequence
+            end_sequence (str | None, optional): the end of the sequence. Defaults to None.
+            sequence_type (str, optional): the type of the sequence.
+                Defaults to "alpha_numerical_both_case".
+            sequence_list (list | str | None, optional): a custom sequence. Defaults to None.
+
+        Raises:
+            ValueError: if sequence_type not in SEQ_TYPES
+            ValueError: if sequence_type == "custom"
+                and (sequence_list not of type list or str | len(sequence_list) < 2)
+            ValueError: if start sequence is > end sequence
         """
         self.sequence_type = sequence_type
 
@@ -75,10 +88,8 @@ class Seq:
 
         if sequence_type not in SEQ_TYPES:
             raise ValueError(
-                'ValueError thrown. Argument "sequence_type" \
-                must be in this list:\n{}'.format(
-                    SEQ_TYPES
-                )
+                f'ValueError thrown. Argument "sequence_type" \
+                must be in this list:\n{SEQ_TYPES}'
             )
         if sequence_type == "custom":
             if not isinstance(sequence_list, (list, str)) or len(sequence_list) < 2:
@@ -109,11 +120,12 @@ class Seq:
         self.current_sequence = self.next_sequence()
         return self.current_sequence
 
-    def next_sequence(self):
-        """'
+    def next_sequence(self) -> str:
+        """
         Sets self.current_sequence to the next sequence
 
-        Returns self.current_sequence
+        Returns:
+            str: self.current_sequence
         """
         index_pos = [
             self.sequence_list.index(str(char)) for char in self.current_sequence
@@ -125,19 +137,27 @@ class Seq:
                 return "".join([self.sequence_list[c] for c in index_pos])
         return self.current_sequence
 
-    def next_char_in_sequence(self, index):
+    def next_char_in_sequence(self, index: int) -> int:
         """
         Helper function to return the next character in the sequence
         given the current index position of the current sequence character
+
+        Args:
+            index (int): the index of a character in self.sequence_list
+
+        Returns:
+            int: the index of the next character in the sequence
         """
         if int(index) == len(self.sequence_list) - 1:
             return 0
         return int(index) + 1
 
-    def sublist(self, *args):
+    def sublist(self, *args) -> list:
         """
-        Helper function to generate different sublists from
-        self.__alpha_num
+        Helper function to generate different sublists from self.__alpha_num
+
+        Returns:
+            list: a sublist self.__alpha_num based on the *args provided
         """
         if len(args) == 2:
             return self.__alpha_num[
@@ -152,9 +172,13 @@ class Seq:
             return ValueError("No end_sequence specified. Sequence length is infinite")
         return self.length
 
-    def get_length(self):
+    def get_length(self) -> int:
         """
-        Helper function to calculate the number of variations between the start and end sequence
+        Helper function to calculate the number of variations between the start and end sequence,
+        inclusive of the start and end sequence
+
+        Returns:
+            int: number of variations between the start and end sequence, inclusive
         """
         # 101 = (1 × 2^0) + (0 × 2^1) + (1 × 2^2) = 5.
         # (value x base array length ^ index pos)
@@ -167,12 +191,9 @@ class Seq:
         end_num = 0
 
         # get the value of each character
-        for i in range(len(self.start_sequence)):
-            start = self.start_sequence[i]
-            start_index = self.sequence_list.index(start)
-
-            end = self.end_sequence[i]
-            end_index = self.sequence_list.index(end)
+        for start_value, end_value in zip(self.start_sequence, self.end_sequence):
+            start_index = self.sequence_list.index(start_value)
+            end_index = self.sequence_list.index(end_value)
 
             num_entry = [start_index, end_index]
             num_list.append(num_entry)
@@ -192,7 +213,7 @@ if __name__ == "__main__":
     end_seq = input("End sequence: ")
     print("")
     for seq_types_index, seq_types_value in enumerate(SEQ_TYPES):
-        print("{}: {}".format(seq_types_index, seq_types_value))
+        print(f"{seq_types_index}: {seq_types_value}")
     seq_type = input(
         "\nPlease enter a sequence type from the above selection\n\
     you can enter the string or the corrisponding number: "
@@ -206,3 +227,4 @@ if __name__ == "__main__":
     print(seq.current_sequence)
     for each in seq:
         print(each)
+    print(f"Total: {len(seq)}")
